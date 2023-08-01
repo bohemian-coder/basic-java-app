4336e23da5e6889de031c1eef8e0be1b0252acfe
bohemian-coder <jenniferejeme@gmail.com>
2023-08-01 12:25:12 -0500

    Update with src code

---
+++ src/test/java/com/may/demo/DemoWorkshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.may.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+
+@SpringBootTest
+class DemoWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+		List li = new ArrayList<String> ();
+	}
+
+}
